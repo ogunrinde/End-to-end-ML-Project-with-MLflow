f9da1bf24b6b18554dc1922babebc007ac8d63e2
Ogunrinde Omotayo <omotayo@steer.global>
2024-04-13 12:59:55 +0100

    implement model tariner

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.mlproject import logger
 from src.mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.mlproject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.mlproject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-
+from src.mlproject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 try:
     STAGE_NAME = "Data Ingestion stage"
     logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
@@ -29,6 +29,17 @@ try:
     STAGE_NAME = "Data Transformation Stage"
     logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+try:
+    STAGE_NAME = "Model Trainer Stage"
+    logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
 except Exception as e:
---
+++ src/mlproject/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,23 @@
+from mlproject.config.configuration import ConfigurationManager
+from mlproject.components.model_trainer import ModelTrainer
+from mlproject import logger
+
+STAGE_NAME = 'Model Train Stage'
+class ModelTrainerTrainingPipeline():
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>> stage {STAGE_NAME} completed <<<<<<<<")
+    except Exception as e:
+        raise e
